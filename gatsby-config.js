95ca3cd780efc947bd9a01100762c085e51f9baa
Be Duc Tai <os.solutionvn@gmail.com>
2021-06-14 00:23:27 +0700

    updates test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `EOSVN Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Be Tai`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    siteUrl: `https://eosvn.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `eosvn`,
     },
   },
   plugins: [
